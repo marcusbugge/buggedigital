3971bf016eef97176fb2a80d3be5e35eb310f9b9
Marcus Bugge <marcus_bugge@hotmail.com>
2025-06-09 23:58:18 +0200

    conflict

--- buggeweb/src/app/sections/navbar/NavHover.jsx
+++ buggeweb/src/app/sections/navbar/NavHover.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
 
-import "./navbar2.scss";
+import "./Navbar2.scss";
 
 export default function NavHover({ isVisible, services, position }) {
   const [hoveredIndex, setHoveredIndex] = useState(null);
